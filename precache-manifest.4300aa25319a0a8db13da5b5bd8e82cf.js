b8f974e6990dac04ec8fe925f1e09e80e64ae259
elyasaf Hershkovitz <Elyuk1988@gmail.com>
2020-04-22 14:17:58 +0300

    Updates

---
+++ precache-manifest.4300aa25319a0a8db13da5b5bd8e82cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "801f3b96f8cc679a6ad6c9f05b710225",
+    "url": "/yousnoozeulose/index.html"
+  },
+  {
+    "revision": "4cd8cdffc2d4daa32f2d",
+    "url": "/yousnoozeulose/static/css/main.96a1e03e.chunk.css"
+  },
+  {
+    "revision": "76e1082df43b8531e522",
+    "url": "/yousnoozeulose/static/js/2.b73c312e.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/yousnoozeulose/static/js/2.b73c312e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cd8cdffc2d4daa32f2d",
+    "url": "/yousnoozeulose/static/js/main.028da60f.chunk.js"
+  },
+  {
+    "revision": "02b6fe3e8fd3b5eda5f0",
+    "url": "/yousnoozeulose/static/js/runtime-main.5422bd6f.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/yousnoozeulose/static/media/image.d7265326.png"
+  }
+]);
